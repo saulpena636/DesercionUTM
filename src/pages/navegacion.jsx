57a85b66fedf0e6f6a6fcd9741ec5dbcde5120c1
saulpena636 <suoerpenniya@gmail.com>
2026-05-14 18:09:20 -0600

    feat: update package.json for legacy support and add browserslist
    - Added @vitejs/plugin-legacy and terser to dependencies.
    - Introduced browserslist configuration for better browser compatibility.
    
    fix: update navigation link text
    
    - Changed link text from "Buscar Estudiante" to "Predicción de estudiantes" for clarity.
    
    chore: enhance Vite configuration for legacy support
    
    - Integrated @vitejs/plugin-legacy for improved compatibility.
    - Set build target to 'es2015' to ensure broader support.

--- src/pages/navegacion.jsx
+++ src/pages/navegacion.jsx
@@ -76,7 +76,7 @@ function Navegacion() {
                     <ul className={styles.navlist}>
                         <li><NavLink to="/" className={({ isActive }) => isActive ? styles.active : ""}><i className="fas fa-square-poll-vertical"></i>Panel principal</NavLink></li>
                         <li><NavLink to="/estudiantes" className={({ isActive }) => isActive ? styles.active : ""}><i className="fas fa-user-graduate"></i>Estudiantes en riesgo</NavLink></li>
-                        <li><NavLink to="/perfil" className={({ isActive }) => isActive ? styles.active : ""}><i className="fas fa-graduation-cap"></i>Buscar Estudiante</NavLink></li>
+                        <li><NavLink to="/perfil" className={({ isActive }) => isActive ? styles.active : ""}><i className="fas fa-graduation-cap"></i>Predicción de estudiantes</NavLink></li>
                         <li><NavLink to="/upload" className={({ isActive }) => isActive ? styles.active : ""}><i className="fas fa-file"></i>Subir Datos</NavLink></li>
                     </ul>
                     
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import legacy from '@vitejs/plugin-legacy'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server:{
-    host: true,
+  plugins: [
+    react(),
+    legacy({
+      // Definimos qué tan atrás queremos llegar en la compatibilidad
+      targets: ['defaults', 'not IE 11', 'edge 18'], 
+      // Polyfills necesarios para que React y Plotly no rompan
+      additionalLegacyPolyfills: ['regenerator-runtime/runtime']
+    })
+  ],
+  // Importante para que el build sea compatible
+  build: {
+    target: 'es2015'
   }
 })
